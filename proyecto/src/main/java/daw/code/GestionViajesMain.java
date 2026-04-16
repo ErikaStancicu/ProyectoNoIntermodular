55790fe53c58402afe35eace6565af284b284646
erika <erikastancicu019@gmail.com>
2026-04-16 22:19:59 +0200

    Edición main

--- proyecto/src/main/java/daw/code/GestionViajesMain.java
+++ proyecto/src/main/java/daw/code/GestionViajesMain.java
@@ -1,18 +1,27 @@
 package daw.code;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class
-GestionViajesMain {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class GestionViajesMain extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/vista_principal.fxml"));
+        Parent root = loader.load();
+
+        stage.setScene(new Scene(root));
+        stage.setWidth(500);
+        stage.setHeight(500);
+        stage.setResizable(false);
+        stage.setTitle("Proyecto No Intermodular");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
